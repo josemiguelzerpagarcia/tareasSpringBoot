5824a326bd7cd927f5c614d98ebae3291c478d01
Jose Miguel Zerpa Garcia <zerpa.garcia.jose.miguel@iescamas.es>
2026-01-19 10:21:32 +0100

    proyecto maven implementado

---
+++ CrudTarea/src/main/java/com/CrudTarea/CrudTarea/CrudTareaApplication.java
@@ -0,0 +1,13 @@
+package com.CrudTarea.CrudTarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudTareaApplication.class, args);
+	}
+
+}
---
+++ CrudTarea/src/test/java/com/CrudTarea/CrudTarea/CrudTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrudTarea.CrudTarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
